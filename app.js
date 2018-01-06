397bcab45c01fdb1beb29f189f7754f7d3f5edb7
匡志斌 <kuangzhibin@live.com>
2018-01-06 18:27:39 +0800

    add database

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ import KoaStatic from 'koa-static'
 import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
 
+import {database} from './mongodb' // 引入mongodb
+import {saveInfo, fetchInfo} from './controlers/info' // 引入info controller
+import {saveStudent, fetchStudent, fetchStudentDetail} from './controlers/student' // 引入 student controller
+
+database()
+
 const app = new Koa()
 const router = new Router()
 
@@ -12,6 +18,15 @@ app.use(KoaStatic(__dirname+ '/public'))
 router.get('/test', (ctx, next) => {
   ctx.body = 'test page'
 })
+
+// 设置每一个路由对应的相对的控制器
+router.post('/saveinfo', saveInfo)
+router.get('/info', fetchInfo)
+
+router.post('/savestudent', saveStudent)
+router.get('/student', fetchStudent)
+router.get('/studentDetail', fetchStudentDetail)
+
 
 app
   .use(router.routes())
---
+++ config/index.js
@@ -0,0 +1,3 @@
+export default {
+  dbPath: 'mongodb://192.168.99.100:32781/graphql'
+}
